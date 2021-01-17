58a7ebe2d9c6c85cd1a5982d7ab7d1c9401659b0
Meghana057 <77562779+Meghana057@users.noreply.github.com>
2021-01-17 10:55:22 +0530

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+require("./credentials.php");
+$con=mysqli_connect($serverAdress,$dbmsUsername,$dbmsPassword,$dbmsName);
+if(mysqli_connect_error())
+{
+    echo "\n connection error";
+}
+?>
