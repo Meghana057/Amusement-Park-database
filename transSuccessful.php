1ca8f1a9ba87418053ccf643b008b5a8028d21a1
Meghana057 <77562779+Meghana057@users.noreply.github.com>
2021-01-17 11:04:58 +0530

    Create transSuccessful.php

---
+++ transSuccessful.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- <link rel="stylesheet" type="text/css" href="admin_login.css"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transaction Successful</title>
+</head>
+<body>
+    <h1>Transaction Successful!</h1><br>
+    <a href="adminPanel.php"><button class="activityButton">Okay</button></a>
+</body>
+</html>
